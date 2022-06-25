965fdeef466ed3bdf46558d82b88bbac53ebb427
chadi7781 <troudishedy6@gmail.com>
2022-06-25 19:00:14 +0000

    register design page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import Logo from "../assets/logo.svg";
+import Logo from "../assets/logo-chat.svg";
 function Register() {
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -15,7 +15,7 @@ function Register() {
     <FormContainer>
       <form onSubmit={(event) => handleSubmit(event)}>
         <div className="brand">
-          <img src={Logo} alt="" style={{width:'20px'}} />
+          <img src={Logo} alt="" />
           <h1>Snappy</h1>
         </div>
         <input
@@ -58,9 +58,76 @@ const FormContainer = styled.div`
   height: 100vh;
   width: 100vw;
   display: flex;
-  flex-direction:   column;
+  flex-direction: column;
   justify-content: center;
-  gap:1rem;
-  
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+
+  .brand {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    justify-content: center;
+    img {
+      height: 3rem;
+      width:3rem;
+
+    }
+    h1 {
+      color:white;
+      text-transform: uppercase;
+    }
+  }
+  form {
+    display:flex;
+    flex-direction: column;
+    gap: 2rem;
+    background-color: #00000076;
+    border-radius: 2rem;
+    padding: 3rem 5rem;
+    input {
+      background-color: transparent;
+      padding: 1rem;
+      border: 0.1rem solid #4e0eff;
+      color: white;
+      border-radius: 0.4rem;
+      width: 100%;  
+      font-size: 1rem;
+
+      &:focus {
+        border:0.1rem solid #997af0;
+        outline: none;
+      }
+    }
+
+    button {
+      background-color: #997af0;
+      color:white;
+      padding:1rem 2rem;
+      border:none;
+      cursor: pointer;
+      font-size: 1rem;
+      text-transform: uppercase;
+      font-weight: bold;
+      border-radius: 0.4rem;
+      transition: 0.2s ease-in-out;
+      &:hover {
+      background-color:#4e0eff;
+    }
+  }
+  span {
+    color:white;
+    text-transform: uppercase;
+
+    a {
+      text-decoration: none;
+      font-weight: bold;
+      color:#4e0eff;
+
+    }
+  }
+    
+  }
 `;
 export default Register;
