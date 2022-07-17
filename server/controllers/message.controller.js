4ddc96aab135f405559b57c88aabca09c9c99d2c
Chadi7781 <troudishedy6@gmail.com>
2022-07-17 06:03:15 +0200

    add function addmessage inside controller

---
+++ server/controllers/message.controller.js
@@ -0,0 +1,23 @@
+const Message = require("../models/message.model");
+
+exports.addMessageController = async (req, res, next) => {
+  try {
+    const { from, to, message } = req.body;
+
+    const data = await Message.create({
+      message: { text: message },
+      users: [from, to],
+      sender: from,
+    });
+
+    if (data) {
+      return res.status(200).json({ message: "Message added successfully" });
+    } else {
+      return res
+        .status(400)
+        .json({ message: "Failed to add message to database." });
+    }
+  } catch (err) {
+    next(err);
+  }
+};
